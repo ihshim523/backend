ea46db699509c58013ef6ab9f08786ae594a36c8
Michael Shim <imapp.help@gmail.com>
2014-05-30 21:40:56 -0700

    .

--- ClipAnywhereServer/server.js
+++ ClipAnywhereServer/server.js
@@ -1,7 +1,23 @@
 #!/bin/env node
 
+var mongo = require('mongojs');
+
 var server  = function(req, res, next) {
-    res.send('test');
+    
+    res.send(req.method);
+    
+    // var db = mongo(connection_string, ['clips']);
+    // var clips = db.collection('clips');
+// 
+//     
+    // MongoClient.connect('mongodb://'+connection_string, function(err, db) {
+      // if(err) throw err;
+      // var collection = db.collection('books').find().limit(10).toArray(function(err, docs) {
+        // console.dir(docs);
+        // db.close();
+      // })
+    // })
+
     next();
 }; 
 
