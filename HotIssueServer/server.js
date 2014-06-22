46da217b25d9ce440eb6e9917ecf5e7f94760ac1
INAPP <imapp.help@gmail.com>
2014-06-21 22:13:34 -0700

    .

--- HotIssueServer/server.js
+++ HotIssueServer/server.js
@@ -1,5 +1,4 @@
 #!/bin/env node
-
 var fs = require('fs');
 var zlib = require('zlibjs');
 
@@ -11,9 +10,17 @@ var post = function(req, res, next) {
     var buffer = new Buffer(req.body.k);
     var compressed = zlib.deflateSync(buffer);
 
-    fs.writeFile('./HotIssue/get.dat', compressed, function(err) {
-        res.send('{"R":"1"}');
-    });
+    try {
+        fs.writeFile('./HotIssue/get.dat', compressed, function(err) {
+            var buffer = new Buffer(req.body.e, 'base64');
+            fs.writeFile('./HotIssue/get2.dat', buffer, function(err) {
+                res.send('{"R":"1"}');
+            });
+        });
+    }
+    catch(e) {
+        next();
+    }
 };
 
 var del = function(req, res, next) {
--- g
+++ g
@@ -1,4 +1,4 @@
 git add .
-git commit -m '.'
+git commit -am '.'
 git push
 
