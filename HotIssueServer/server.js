9582da278005e32e8c979602e84f9b801969bd6d
Michael Shim <imapp.help@gmail.com>
2014-12-12 03:00:48 -0800

    .

--- HotIssueServer/server.js
+++ HotIssueServer/server.js
@@ -36,21 +36,27 @@ var get = function(req, res, next) {
 	    	    var compressed = zlib.deflateSync(buffer);
 	    	    res.send(compressed);
 	    	}
-	    	else
+	    	else {
+                console.log('get1:'+err);
 	    		next();
+            }
 	    });
 	}
 	catch(e) {
+        console.log('get2:'+e);
 		next();
 	}
 
 };
 
 var post = function(req, res, next) {
-	var client = naver({key: '5b3cf79266a7f34b2d102a8161826d59'});
+	var client = naver({key: '1e7189070640781bb5e7fae3c7f88904'});
     var hotissue = db.collection('hotissue');
 
-    var obj = JSON.parse(req.body.k);
+// console.log('#####'+JSON.stringify(req.body)+'#####');
+
+//    var obj = JSON.parse(req.body.k);
+    var obj = req.body.k;
 
     //console.log('#### series begin');
     
@@ -71,20 +77,20 @@ var post = function(req, res, next) {
             });
     	},
     	function(cb2){
-    		//console.log("BING0:::"+item.title);
+    		console.log("BING0:::"+item.title);
     	    client.search(item.title, {target:'news'}, function(error, response, data) {
-    	    	if (!error) {
-    	    		//console.log("BING4:::" + JSON.stringify(data));
+                if ( !data.error ) {
+    	    		console.log("BING4:::" + JSON.stringify(data));
     	    		cb2(null, data);
     	    	}
     	    	else {
-    	    		//console.log("BING1:::"+error);
+    	    		console.log("BING1:::"+error);
     	    		cb2(true);
     	    	}
     	    });
     	},
     	function(data, cb2){
-    		//console.log("BING2:::");//+JSON.stringify(data));
+    		console.log("BING2:::");//+JSON.stringify(data));
     		if ( data && data.rss && data.rss.channel && data.rss.channel.length > 0 && 
     				data.rss.channel[0].item && data.rss.channel[0].item.length > 0 &&
     				data.rss.channel[0].item[0].description && data.rss.channel[0].item[0].description.length > 0 ) {
@@ -93,7 +99,7 @@ var post = function(req, res, next) {
     			    wordwrap: 130
     			});
 
-	    		//console.log("BING3:: "+item.title+" :::"+text);
+	    		console.log("BING3:: "+item.title+" :::"+text);
 
 		        hotissue.update({k:item.title},{k:item.title,v:text,
 		        	expire:(new Date).getTime()}, {upsert:true}, function(err, saved) { //
@@ -106,27 +112,36 @@ var post = function(req, res, next) {
     	}
     	],
     	function(err){
-      		//console.log('#### next');
+
+            // hotissue.update({k:item.title},{k:item.title,v:text,
+            //     expire:(new Date).getTime()}, {upsert:true}, function(err, saved) { //
+            //         //if ( err ) console.log(err);
+            //         cb2();
+            //     });
     		
     		cb();
     	}
     	);
     },
     function(err){
-  		//console.log('#### series end');
-    	
-        var buffer = new Buffer(req.body.k);
-        var compressed = zlib.deflateSync(buffer);
+  		console.log('#### series end');
+        try {	
+            var buffer = new Buffer(JSON.stringify(req.body.k));
+            var compressed = zlib.deflateSync(buffer);
 
-        try {
+            console.log('#### series end 1');
+
             fs.writeFile('./HotIssue/get.dat', compressed, function(err) {
-                var buffer = new Buffer(req.body.e, 'base64');
-                fs.writeFile('./HotIssue/get2.dat', buffer, function(err) {
-                    res.send('{"R":"1"}');
-                });
+                console.log('#### series end 2:'+err);
+                // var buffer = new Buffer(req.body.e, 'base64');
+                // fs.writeFile('./HotIssue/get2.dat', buffer, function(err) {
+                //     res.send('{"R":"1"}');
+                // });
+                res.send('{"R":"1"}');
             });
         }
         catch(e) {
+            console.log('#### series ex:' + e);
             next();
         }
     });
