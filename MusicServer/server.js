cf3892e51c82496cedc25bbfd887b4acd746eb50
Michael Shim <imapp.help@gmail.com>
2015-01-15 23:41:05 -0800

    .

--- MusicServer/server.js
+++ MusicServer/server.js
@@ -4,13 +4,14 @@ var fs = require('fs');
 var zlib = require('zlibjs');
 var async = require('async');
 var mongo = require('mongojs');
+var lz = require('lz-string');
+
 var connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
   process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
   process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
   process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
   process.env.OPENSHIFT_APP_NAME;
 var db = mongo(connection_string, ['music']);
-
 //////////////////////////////////
 var init = function() {
 };
@@ -42,13 +43,109 @@ var post = function(req, res, next) {
 
     var buffer = new Buffer(req.body.k);
 	
-	//console.log(JSON.stringify(req.body.k) + '###'+buffer.length);
-	
+    var obj = JSON.parse(req.body.k);
+
     var compressed = zlib.deflateSync(buffer);
 
     try {
-        fs.writeFile('./Music/get.dat', compressed, function(err) {
-            res.send('{"R":"1"}');
+        async.waterfall([
+        function(cb){
+            fs.writeFile('./Music/get.dat', compressed, function(err) {
+                cb(null);
+            });
+        },
+        function(cb) {
+            var ranking = {rank:[]};
+
+            obj.rank.forEach(function(item){
+                if ( item.site === 'melon' )
+                    ranking.rank.push(item);
+            });
+
+            var compressed = lz.compressToUTF16(JSON.stringify(ranking));
+            fs.writeFile('./Music/melon.dat', compressed, function(err) {
+                fs.writeFile('./Music/melon5.dat', compressed, function(err) {
+                    cb(null);
+                });
+            });
+        },
+        function(cb) {
+            var ranking = {rank:[]};
+
+            obj.rank.forEach(function(item){
+                if ( item.site === 'mnet' )
+                    ranking.rank.push(item);
+            });
+
+            var compressed = lz.compressToUTF16(JSON.stringify(ranking));
+            fs.writeFile('./Music/mnet.dat', compressed, function(err) {
+                fs.writeFile('./Music/mnet5.dat', compressed, function(err) {
+                    cb(null);
+                });
+            });
+        },
+        function(cb) {
+            var ranking = {rank:[]};
+
+            obj.rank.forEach(function(item){
+                if ( item.site === 'bugs' )
+                    ranking.rank.push(item);
+            });
+
+            var compressed = lz.compressToUTF16(JSON.stringify(ranking));
+            fs.writeFile('./Music/bugs.dat', compressed, function(err) {
+                fs.writeFile('./Music/bugs5.dat', compressed, function(err) {
+                    cb(null);
+                });
+            });
+        },
+        function(cb) {
+            var ranking = {rank:[]};
+
+            obj.rank.forEach(function(item){
+                if ( item.site === 'soribada' )
+                    ranking.rank.push(item);
+            });
+
+            var compressed = lz.compressToUTF16(JSON.stringify(ranking));
+            fs.writeFile('./Music/soribada.dat', compressed, function(err) {
+                fs.writeFile('./Music/soribada5.dat', compressed, function(err) {
+                    cb(null);
+                });
+            });
+        },
+        function(cb) {
+            var ranking = {rank:[]};
+
+            obj.rank.forEach(function(item){
+                if ( item.site === 'dosirak' )
+                    ranking.rank.push(item);
+            });
+
+            var compressed = lz.compressToUTF16(JSON.stringify(ranking));
+            fs.writeFile('./Music/dosirak.dat', compressed, function(err) {
+                fs.writeFile('./Music/dosirak5.dat', compressed, function(err) {
+                    cb(null);
+                });
+            });
+        },
+        function(cb) {
+            var ranking = {rank:[]};
+
+            obj.rank.forEach(function(item){
+                if ( item.site === 'billboard' )
+                    ranking.rank.push(item);
+            });
+
+            var compressed = lz.compressToUTF16(JSON.stringify(ranking));
+            fs.writeFile('./Music/billboard.dat', compressed, function(err) {
+                fs.writeFile('./Music/billboard5.dat', compressed, function(err) {
+                    cb(null);
+                });
+            });
+        }],
+        function(err) {
+             res.send('{"R":"1"}');
         });
     }
     catch(e) {
