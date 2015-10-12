0a7e92a4adde2186e430bfd4a7133acb914f102c
Michael Shim <imapp.help@gmail.com>
2015-10-12 15:20:46 -0700

    .

--- VideoServer/server.js
+++ VideoServer/server.js
@@ -11,133 +11,134 @@ var lz = require('lz-string');
 //   process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
 //   process.env.OPENSHIFT_APP_NAME;
 
-var db;
+var db = require('../elastic.js');
 //////////////////////////////////
-var init = function(cb, mongo) {
-	db = mongo;
-  cb(null);
-};
 
 var get = function(req, res, next) {
-	var video = db.collection('video');
-
-	try{
-	    video.findOne({k:req.query.k}, function(err, doc) {
-	    	if (!err) {
-	    		//console.log("DOC:::"+JSON.stringify(doc));
-
-	    	    var input = new Buffer(JSON.stringify(doc));
-	    	    zlib.deflate(input, function(err, compressed){
-					if (!err) {
-			    	    res.send(compressed);
-					}
-					else
-						next();
-				});
-	    	}
-	    	else
-	    		next();
-	    });
-	}
-	catch(e) {
-		next();
-	}
-
+	res.end();
 };
 
-var post = function(req, res, next) {
-    var video = db.collection('video');
+function makeFile(input, obj, cb2){
+	zlib.deflate(input, function(err, compressed){
+	try {
+		async.waterfall([
+		function(cb){
+			fs.writeFile('./Video/get.dat', compressed, function(err) {
+				cb(null);
+			});
+		},
+		function(cb) {
+			var ranking = {rank:[]};
 
-    var input = new Buffer(req.body.k);
+			obj.rank.forEach(function(item){
+				if ( item.site === 'nate' )
+					ranking.rank.push(item);
+			});
 
-    var obj = JSON.parse(req.body.k);
+			var compressed = lz.compressToUTF16(JSON.stringify(ranking));
+			fs.writeFile('./Video/v1.dat', compressed, function(err) {
+				cb(null);
+			});
+		},
+		function(cb) {
+			var ranking = {rank:[]};
 
-    zlib.deflate(input, function(err, compressed){
-		try {
-			async.waterfall([
-			function(cb){
-				fs.writeFile('./Video/get.dat', compressed, function(err) {
+			obj.rank.forEach(function(item){
+				if ( item.site === 'youtube' )
+					ranking.rank.push(item);
+			});
+
+			var compressed = lz.compressToUTF16(JSON.stringify(ranking));
+			fs.writeFile('./Video/v2.dat', compressed, function(err) {
+				cb(null);
+			});
+		},
+		function(cb) {
+			var ranking = {rank:[]};
+
+			obj.rank.forEach(function(item){
+				if ( item.site === 'pandora' )
+					ranking.rank.push(item);
+			});
+
+			var compressed = lz.compressToUTF16(JSON.stringify(ranking));
+			fs.writeFile('./Video/v3.dat', compressed, function(err) {
+				cb(null);
+			});
+		},
+		function(cb) {
+			var ranking = {rank:[]};
+
+			obj.rank.forEach(function(item){
+				if ( item.site === 'mgoon' )
+					ranking.rank.push(item);
+			});
+
+			var compressed = lz.compressToUTF16(JSON.stringify(ranking));
+			fs.writeFile('./Video/v4.dat', compressed, function(err) {
 					cb(null);
-				});
-			},
-			function(cb) {
-				var ranking = {rank:[]};
+			});
+		},
+		function(cb) {
+			var ranking = {rank:[]};
 
-				obj.rank.forEach(function(item){
-					if ( item.site === 'nate' )
-						ranking.rank.push(item);
-				});
+			obj.rank.forEach(function(item){
+				if ( item.site === 'afreeca' )
+					ranking.rank.push(item);
+			});
 
-				var compressed = lz.compressToUTF16(JSON.stringify(ranking));
-				fs.writeFile('./Video/v1.dat', compressed, function(err) {
-					cb(null);
-				});
-			},
-			function(cb) {
-				var ranking = {rank:[]};
-
-				obj.rank.forEach(function(item){
-					if ( item.site === 'youtube' )
-						ranking.rank.push(item);
-				});
-
-				var compressed = lz.compressToUTF16(JSON.stringify(ranking));
-				fs.writeFile('./Video/v2.dat', compressed, function(err) {
-					cb(null);
-				});
-			},
-			function(cb) {
-				var ranking = {rank:[]};
-
-				obj.rank.forEach(function(item){
-					if ( item.site === 'pandora' )
-						ranking.rank.push(item);
-				});
-
-				var compressed = lz.compressToUTF16(JSON.stringify(ranking));
-				fs.writeFile('./Video/v3.dat', compressed, function(err) {
-					cb(null);
-				});
-			},
-			function(cb) {
-				var ranking = {rank:[]};
-
-				obj.rank.forEach(function(item){
-					if ( item.site === 'mgoon' )
-						ranking.rank.push(item);
-				});
-
-				var compressed = lz.compressToUTF16(JSON.stringify(ranking));
-				fs.writeFile('./Video/v4.dat', compressed, function(err) {
-						cb(null);
-				});
-			},
-			function(cb) {
-				var ranking = {rank:[]};
-
-				obj.rank.forEach(function(item){
-					if ( item.site === 'afreeca' )
-						ranking.rank.push(item);
-				});
-
-				var compressed = lz.compressToUTF16(JSON.stringify(ranking));
-				fs.writeFile('./Video/v5.dat', compressed, function(err) {
-					cb(null);
-				});
-			}
-			],
-			function(err) {
-				 res.send('{"R":"1"}');
+			var compressed = lz.compressToUTF16(JSON.stringify(ranking));
+			fs.writeFile('./Video/v5.dat', compressed, function(err) {
+				cb(null);
 			});
 		}
-		catch(e) {
-			next();
-		}
-	});
+		],
+		function(err) {
+			 cb(null);
+		});
+	}
+	catch(e) {
+		console.log(JSON.stringify(e));
+		cb(true);
+	}
+}
+
+var post = function(req, res, next) {
+    var input = new Buffer(req.body.k);
+    var obj = JSON.parse(req.body.k);
+
+		makeFile(input, obj, function(err){
+			if (!err)
+				res.send('{"R":"1"}');
+			res.end();
+
+			db.index({index:'hotissue', type:'temp_video', id:'all', body:req.body.k}, function(err){
+			});
+		})
 };
 
 var del = function(req, res, next) {
-    next();
+    res.end();
+};
+
+function list(cb) {
+	db.get({index:'hotissue', type:'temp_video', id:'all'}, function(err, doc){
+		if (!err && doc.found){
+			var obj = doc._source;
+			var input = new Buffer(obj);
+			makeFile(input, obj, function(err){
+				cb(null);
+			});
+		}
+	});
+}
+
+var init = function(cb) {
+	setInterval(function(){
+    list(function(){});
+  },60000);
+
+	list(cb);
 };
 
 var server  = function(req, res, next) {
